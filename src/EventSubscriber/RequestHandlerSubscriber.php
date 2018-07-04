a0e93ae7f492f64c44f2894d95f6998847d1c4bf
anand.toshniwal <anand.toshniwal1993@gmail.com>
2018-07-04 16:42:35 +0530

    Activity-23: Event api extended.

--- src/EventSubscriber/RequestHandlerSubscriber.php
+++ src/EventSubscriber/RequestHandlerSubscriber.php
@@ -5,7 +5,9 @@ namespace Drupal\d8_training\EventSubscriber;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\Core\Logger\LoggerChannelFactory;
 use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\d8_training\Event\NodeInsertEvent;
 
 /**
  * Event Subscriber RequestHandlerSubscriber.
@@ -20,13 +22,20 @@ class RequestHandlerSubscriber implements EventSubscriberInterface {
   protected $routeMatch;
 
   /**
-   * Constructs a new RouteCacheContext class.
+   * The logger service.
    *
-   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
-   *   The route match.
+   * @var \Drupal\Core\Logger\LoggerChannelFactory
    */
-  public function __construct(RouteMatchInterface $route_match) {
+    protected $logger;
+
+  /**
+   * RequestHandlerSubscriber constructor.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   * @param \Drupal\Core\Logger\LoggerChannelFactory $logger
+   */
+  public function __construct(RouteMatchInterface $route_match, LoggerChannelFactory $logger) {
     $this->routeMatch = $route_match;
+    $this->logger = $logger->get('d8_training');
   }
 
   /**
@@ -34,6 +43,7 @@ class RequestHandlerSubscriber implements EventSubscriberInterface {
    */
   public static function getSubscribedEvents() {
     $events[KernelEvents::RESPONSE][] = ['onRespond'];
+    $events[NodeInsertEvent::NODE_INSERT][] = ['onNodeInsert'];
     return $events;
   }
 
@@ -41,9 +51,16 @@ class RequestHandlerSubscriber implements EventSubscriberInterface {
    * {@inheritdoc}
    */
   public function onRespond(FilterResponseEvent $event) {
-    if($this->routeMatch->getRouteName() == 'entity.node.canonical') {
+    if ($this->routeMatch->getRouteName() == 'entity.node.canonical') {
       $response = $event->getResponse();
       $response->headers->set('Access-Control-Allow-Origin', '*');
     }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function onNodeInsert(NodeInsertEvent $events) {
+    $this->logger->notice('Node ' . $events->getEntity()->getTitle() . ' has been created.');
   }
 }

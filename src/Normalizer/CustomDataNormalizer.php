f77c7ef8ab91b522f7f353c8fbcd71c143315358
Mac <mac@Macs-MacBook-Air.local>
2018-07-18 16:38:33 +0530

    Extra-4: Convert the output of default REST endpoint into a simplified structure.

---
+++ src/Normalizer/CustomDataNormalizer.php
@@ -0,0 +1,26 @@
+<?php
+namespace Drupal\d8_training\Normalizer;
+use Drupal\serialization\Normalizer\ContentEntityNormalizer;
+use Drupal\Core\Datetime\DrupalDateTime;
+
+class CustomDataNormalizer extends ContentEntityNormalizer{
+ 
+  /**
+ * The interface or class that this Normalizer supports.
+ *
+ * @var string
+ */
+  protected $supportedInterfaceOrClass = 'Drupal\node\NodeInterface';
+/**
+ * {@inheritdoc}
+ */
+  public function normalize($entity, $format = NULL, array $context = array()) {
+    $attributes = parent::normalize($entity, $format, $context);
+    foreach ($attributes as $key => $value) {
+      if(isset($attributes[$key][0]['value'])){
+        $attributes[$key] = $attributes[$key][0]['value'];
+      }
+    }
+    return $attributes;
+  }
+}
